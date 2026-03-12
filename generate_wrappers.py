0dddbd067c96297ec5aa590f6e536d5a40abb9c1
Luc LAURENT <luc.laurent@cnam.fr>
2026-03-12 14:52:26 +0100

    old .pyx files and update wrapper generator

--- generate_wrappers.py
+++ generate_wrappers.py
@@ -1,10 +1,23 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).parent / 'mumps'
+_BASE_DIR = Path(__file__).parent / 'src'
 
-with open(BASE_DIR / '_mumps.tpl', 'rt') as f:
-    template = f.read()
+def generate_wrappers(type_num = ['s', 'd', 'c', 'z'], version="5.8.x", src_dir=_BASE_DIR):
 
-for x in {'s', 'd', 'c', 'z'}:
-    with open(BASE_DIR / f'_{x}mumps.pyx', 'wt') as f:
-        f.write(template.format(x=x, X=x.upper()))
+    if isinstance(type_num, str):
+        type_num = [type_num]
+        
+    # adapted version
+    formatted_version  = version[:3] + '.x'
+    # template
+    _MUMPS_TEMPLATE = src_dir / ('_mumps_{}.tpl'.format(formatted_version))
+
+    with open(_MUMPS_TEMPLATE, 'rt') as f:
+        template = f.read()
+
+    for x in type_num:
+        with open(src_dir / f'_{x}mumps.pyx', 'wt') as f:
+            f.write(template.format(x=x, X=x.upper()))
+            
+if __name__ == '__main__':
+    generate_wrappers()
